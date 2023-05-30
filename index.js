2793b3f0033c84a0faae994ab7f00f9135a1b367
Nii Odoi <niiodoi2@gmail.com>
2023-05-30 17:24:58 +0100

    js finished

---
+++ index.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('forms');
+const name = document.querySelector('#name');
+const email = document.querySelector('#email');
+const error = document.querySelector('.error-message');
+const btn = document.querySelector('.btn');
+
+function vanish() {
+    error.style.display = 'none';
+}
+
+btn.addEventListener('click', function(e){
+    e.preventDefault();
+
+    if(name.value === '' ||  email.value === ''){
+        error.style.display = 'block';
+        setTimeout(vanish, 5000)
+        return;
+    }
+    let result = `Hello ${name.value}🎊👨‍💻,  your form has been submitted successfully. Your email is ${email.value}`
+    alert(result);
+
+    name.value = ''
+    email.value = ''
+    
+});

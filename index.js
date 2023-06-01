9b190ebae819588fe4b67ebb69730b17dbf52074
Nii Odoi <niiodoi2@gmail.com>
2023-06-01 02:04:13 +0100

    final project

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const form = document.querySelector('forms');
-const name = document.querySelector('#name');
-const email = document.querySelector('#email');
+const form = document.querySelector('.forms');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
 const error = document.querySelector('.error-message');
 const btn = document.querySelector('.btn');
 
@@ -16,10 +16,10 @@ btn.addEventListener('click', function(e){
         setTimeout(vanish, 5000)
         return;
     }
-    let result = `Hello ${name.value}🎊👨‍💻,  your form has been submitted successfully. Your email is ${email.value}`
+    let result = `Hello ${name.value}🎊👨‍💻, your form has been submitted successfully. Your email is ${email.value}`
     alert(result);
 
-    name.value = ''
-    email.value = ''
+    name.value = '';
+    email.value = '';
     
 });
